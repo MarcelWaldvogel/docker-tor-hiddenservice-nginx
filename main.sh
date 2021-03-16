97120de32bae69b3154014682ab704529e0278e7
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2021-03-16 14:44:18 +0100

    :goal_net: Be more careful about overwriting files
    Driveby: Clean files, messages

--- main.sh
+++ main.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+# Usage:
+# 1. main.sh generate <regex for onion address>
+# 2. main.sh serve
+
+function save() {
+  [ -f "$1" ] && mv "$1" "$1".`date +%s`-$RANDOM
+}
 
 if [ "$1" == "generate" ]
 then
     if [ -f /web/private_key ]
     then
         echo '[-] You already have an private key, delete it if you want to generate a new key'
-        exit -1
+        exit 1
     fi
     if [ -z "$2" ]
     then
-        echo '[-] You dont provided any mask, please inform an mask to generate your address'
-        exit -1
+        echo '[-] You did not provide any mask, please add a mask to generate your address'
+        exit 1
     else
         echo '[+] Generating the address with mask: '$2
         shallot -f /tmp/key $2
@@ -21,19 +28,22 @@ then
     address=$(grep Found /tmp/key | cut -d ':' -f 2 )
 
     echo '[+] Generating nginx configuration for site '$address
-    echo 'server {' > /web/site.conf
-    echo '  listen 127.0.0.1:8080;' >> /web/site.conf
-    echo '  root /web/www/;' >> /web/site.conf
-    echo '  index index.html index.htm;' >> /web/site.conf
-    echo '  server_name '$address';' >> /web/site.conf
-    echo '}' >> /web/site.conf
-
+    save /web/site.conf
+    cat << EOF > /web/site.conf
+server {
+  listen 127.0.0.1:8080;
+  root /web/www/;
+  index index.html index.htm;
+  server_name '$address';
+}
+EOF
     echo '[+] Creating www folder'
-    mkdir /web/www
+    mkdir -p /web/www
     chmod 755 /web/
     chmod 755 /web/www
     echo '[+] Generating index.html template'
-    echo '<html><head><title>Your very own hidden service is ready</title></head><body><h1>Well done !</h1></body></html>' > /web/www/index.html
+    save /web/www/index.html
+    echo '<html><head><title>Your very own hidden service is ready</title></head><body><h1>Well done!</h1></body></html>' > /web/www/index.html
     chown hidden:hidden -R /web/www
 fi
 
@@ -41,13 +51,12 @@ if [ "$1" == "serve" ]
 then
     if [ ! -f /web/private_key ]
     then
-        echo '[-] Please run this container with generate argument to initialize your web page'
-        exit -1
+        echo '[-] Please run this container with "generate" argument to initialize your web site'
+	echo '[-] (e.g., docker run tor generate ^pattern)'
+        exit 1
     fi
     echo '[+] Starting tor'
     tor -f /etc/tor/torrc &
     echo '[+] Starting nginx'
-    nginx &
-    # Monitor logs
-    sleep infinity
+    exec nginx
 fi
